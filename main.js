f100a810d04d130b5d48ee049bdab917a0d6e01c
arcmorales <arcmorales86@gmail.com>
2017-05-17 17:27:37 +0800

    :bug: fix(validation): remove required fields on login

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $( document ).ready( function () {
 	var SAMPLE_USER = {
-		'test'  : 'password',
-		'test2' : 'password2'
+		'ara.morales'	: 'InterGalactic9?',
+		'A_morales'		: '&PazWorD'
 	};
 	var has_error = window.location.search.substring( 1 ) === 'error';
 
 	has_error && show_error();
 	$( '.js_form-signin' ).submit( process_form );
 
-	function show_error () {
+	function show_error() {
 		$( '.js_errormsg' ).text( 'Invalid username or password' );
 		$( '.js_erroralert' ).removeClass( 'hidden' )
 	}
 
-	function process_form ( e ) {
+	function process_form( e ) {
 		e.preventDefault();
 		var username = $( '.js_user' ).val();
 		var password = $( '.js_password' ).val();
